71aa1cc392f8810388c854e3fba580fe9f4f44b8
Evgeny Boger <boger@contactless.ru>
2018-04-17 12:54:14 +0300

    Adds option to allow whitelisting certain destinations

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 Simple socks5 server using go-socks5 with auth
 
 # Start container with proxy
-```docker run -d --name socks5-proxy -p 1080:1080 -e PROXY_USER=<PROXY_USER> -e PROXY_PASSWORD=<PROXY_PASSWORD> serjs/go-socks5-proxy```
+```docker run -d --name socks5-proxy -p 1080:1080 -e PROXY_USER=<PROXY_USER> -e PROXY_PASSWORD=<PROXY_PASSWORD> -e PROXY_DEST_WHITELIST=<PROXY_DEST_WHITELIST> serjs/go-socks5-proxy```
 
 where
 
@@ -12,5 +12,10 @@ where
 
 ```<PROXY_PASSWORD>``` - password to authenticate
 
+```<PROXY_DEST_WHITELIST>``` - space separated list of whitelisted destinations of form ```<IP>[/net]|<hostname glob>[:port]```
+
+# Example whitelist
+
+PROXY_DEST_WHITELIST="52.58.0.0/15 *.telegram.org:443"
 # Test running service
 ```curl --socks5 <docker machine ip>:1080 -U <PROXY_USER>:<PROXY_PASSWORD> https://ifcfg.me``` - result must show docker host ip (for bridged network)
--- server.go
+++ server.go
@@ -1,20 +1,109 @@
 package main
 
 import (
-	"github.com/armon/go-socks5"
-	"os"
+	"fmt"
 	"log"
+	"net"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/armon/go-socks5"
+	"github.com/gobwas/glob"
+	"golang.org/x/net/context"
 )
 
+type ServerRuleSet struct {
+	Whitelist []FilterSpec
+}
+
+func (p *ServerRuleSet) Allow(ctx context.Context, req *socks5.Request) (context.Context, bool) {
+	for _, v := range p.Whitelist {
+		if v.Port != 0 {
+			if v.Port != req.DestAddr.Port {
+				continue
+			}
+		}
+
+		if v.IPNet.IP != nil {
+			if v.IPNet.Contains(req.DestAddr.IP) {
+				fmt.Printf("pass: dest ip %v matches whitelisted subnet %v\n", req.DestAddr.IP, v.IPNet)
+				return ctx, true
+			}
+		}
+
+		if v.HostnameGlob != nil {
+			if v.HostnameGlob.Match(req.DestAddr.FQDN) {
+				fmt.Printf("pass: dest FQDN %s matches whitelisted hostname glob %v\n", req.DestAddr.FQDN, v.HostnameGlob)
+				return ctx, true
+			}
+
+		}
+
+	}
+	fmt.Printf("reject: FDQN: %s IP: %s PORT: %d\n", req.DestAddr.FQDN, req.DestAddr.IP, req.DestAddr.Port)
+	return ctx, false
+}
+
+type FilterSpec struct {
+	HostnameGlob glob.Glob
+	IPNet        net.IPNet
+	Port         int
+}
+
+func ParseFilterList(s string) []FilterSpec {
+	filterSpecList := []FilterSpec{}
+	specParts := strings.Split(s, " ")
+	for _, v := range specParts {
+		destParts := strings.SplitN(v, ":", 2)
+		host := destParts[0]
+		if len(host) > 0 {
+			filterSpec := FilterSpec{}
+
+			if len(destParts) == 2 {
+				if i, err := strconv.Atoi(destParts[1]); err == nil {
+					filterSpec.Port = i
+				}
+
+			}
+			// parse as CIDR (e.g. 1.2.3/24) first
+			_, ipNet, err := net.ParseCIDR(host)
+			if err == nil {
+				filterSpec.IPNet = *ipNet
+			} else {
+				// then try to parse as IP address
+				ip := net.ParseIP(host)
+				if ip != nil {
+					filterSpec.IPNet.IP = ip
+					filterSpec.IPNet.Mask = net.IPMask{0xff, 0xff, 0xff, 0xff}
+				} else {
+					// if it's not an IP then assume it's FQDN
+					filterSpec.HostnameGlob = glob.MustCompile(host, '.')
+				}
+
+			}
+
+			filterSpecList = append(filterSpecList, filterSpec)
+		}
+
+	}
+	return filterSpecList
+}
+
 func main() {
+	whitelist := ParseFilterList(os.Getenv("PROXY_DEST_WHITELIST"))
 	creds := socks5.StaticCredentials{
-		os.Getenv("PROXY_USER"):os.Getenv("PROXY_PASSWORD"),
+		os.Getenv("PROXY_USER"): os.Getenv("PROXY_PASSWORD"),
 	}
 	cator := socks5.UserPassAuthenticator{Credentials: creds}
 	// Create a SOCKS5 server
 	conf := &socks5.Config{
 		AuthMethods: []socks5.Authenticator{cator},
 		Logger:      log.New(os.Stdout, "", log.LstdFlags),
+	}
+
+	if len(whitelist) > 0 {
+		conf.Rules = &ServerRuleSet{whitelist}
 	}
 	server, err := socks5.New(conf)
 	if err != nil {
